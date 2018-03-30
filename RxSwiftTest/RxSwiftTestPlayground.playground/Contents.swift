ce30d6acbd593ef6d9c6c8d132a61ddfe3666e84
DimMalysh <d.malysh@mail.ru>
2018-03-30 16:07:25 +0300

    Intro in RxSwift
    Intro in Subjects in playground

--- RxSwiftTest/RxSwiftTestPlayground.playground/Contents.swift
+++ RxSwiftTest/RxSwiftTestPlayground.playground/Contents.swift
@@ -67,7 +67,7 @@ example("dispose") {
 */
 
 /*
-example("disposeBag") {
+example("DisposeBag") {
     let disposeBug = DisposeBag()
     let seq = [1, 2, 3]
     let subscription = Observable.from(seq)
@@ -111,6 +111,7 @@ example("map") {
 }
 */
 
+/*
 example("merge") {
     let firstSeq = Observable.of(1, 2, 3)
     let secondSeq = Observable.of(4, 5, 6)
@@ -121,3 +122,96 @@ example("merge") {
         print(event)
     })
 }
+*/
+
+//MARK: Subjects
+
+/*
+example("PublishSubject") {
+    let disposeBug = DisposeBag()
+    let publishSubject = PublishSubject<String>()
+    
+    publishSubject.subscribe({ (event) in
+        print("Subscription first:", event)
+    }).addDisposableTo(disposeBug)
+    
+    enum MyError: Error {
+        case test
+    }
+    
+    publishSubject.on(.next("Hello"))
+    //publishSubject.onCompleted()
+    //publishSubject.onError(MyError.test)
+    publishSubject.onNext("RxSwift")
+    
+    publishSubject.subscribe(onNext: { (event) in
+        print("Subscription second:", event)
+    }).addDisposableTo(disposeBug)
+    
+    publishSubject.onNext("Hi!")
+    publishSubject.onNext("Bye!")
+}
+*/
+
+/*
+example("BehaviorSubject") {
+    let disposeBug = DisposeBag()
+    let behaviorSubject = BehaviorSubject(value: 1) //[1]
+
+    let firstSubscription = behaviorSubject.subscribe(onNext: { (event) in
+        print(#line, event)
+    }).addDisposableTo(disposeBug)
+
+    behaviorSubject.onNext(2) //[1, 2]
+    behaviorSubject.onNext(3) //[1, 2, 3]
+    
+    let secondSubscription = behaviorSubject.subscribe(onNext: { (event) in
+        print(#line, event) //[3]
+    }).addDisposableTo(disposeBug)
+}
+*/
+
+/*
+example("ReplaySubject") {
+    let disposeBug = DisposeBag()
+    let replaySubject = ReplaySubject<String>.create(bufferSize: 1)
+    
+    replaySubject.subscribe(onNext: { (event) in
+        print("Subscription first:", event)
+    }).addDisposableTo(disposeBug)
+    
+    replaySubject.onNext("a")
+    replaySubject.onNext("b")
+    
+    replaySubject.subscribe(onNext: { (event) in
+        print("Subscription second:", event)
+    }).addDisposableTo(disposeBug)
+
+    replaySubject.onNext("c")
+    replaySubject.onNext("d")
+    
+    
+    let anotherReplaySubject = ReplaySubject<Int>.create(bufferSize: 3)
+    
+    anotherReplaySubject.onNext(1)
+    anotherReplaySubject.onNext(2)
+    anotherReplaySubject.onNext(3)
+    anotherReplaySubject.onNext(4)
+    
+    anotherReplaySubject.subscribe(onNext: { (event) in
+        print("Subscription another first:", event)
+    }).addDisposableTo(disposeBug)
+}
+*/
+
+example("Variables") {
+    let disposeBug = DisposeBag()
+    let variable = Variable("A")
+    
+    variable.asObservable().subscribe(onNext: { (event) in
+        print(event)
+    }).addDisposableTo(disposeBug)
+    
+    variable.value = "B"
+}
+
